f084558ffedb07799f8377a30a38cd0d2aa574c9
Francis Hwang <sera@fhwang.net>
2010-05-07 08:34:38 -0400

    refactor

--- lib/sample_models/sampler.rb
+++ lib/sample_models/sampler.rb
@@ -115,63 +115,77 @@ module SampleModels
     class Finder
       def initialize(model, attrs)
         @model, @attrs = model, attrs.clone
+        @ar_query = ARQuery.new
       end
-    
-      def add_has_many_subselect(value, assoc, find_query)
-        if value.empty?
-          not_matching_subselect = @model.construct_finder_sql(
-            :select => "#{@model.table_name}.id", :joins => assoc.name,
-            :group => "#{@model.table_name}.id"
-          )
-          find_query.condition_sqls << "id not in (#{not_matching_subselect})"
-        else
-          matching_inner_subselect = @model.construct_finder_sql(
-            :select =>
-              "#{@model.table_name}.id, count(#{assoc.klass.table_name}.id) as count",
-            :joins => assoc.name,
-            :conditions => [
-              "#{assoc.klass.table_name}.id in (?)", value.map(&:id)
-            ],
-            :group => "#{@model.table_name}.id"
-          )
-          matching_subselect =
-            "id in (select matching.id from (#{matching_inner_subselect}) as matching where matching.count = #{value.size})"
-          find_query.condition_sqls << matching_subselect
-          not_matching_subselect = @model.construct_finder_sql(
-            :select => "#{@model.table_name}.id", :joins => assoc.name,
-            :conditions => [
-              "#{assoc.klass.table_name}.id not in (?)", value.map(&:id)
-            ],
-            :group => "#{@model.table_name}.id"
-          )
-          find_query.condition_sqls << "id not in (#{not_matching_subselect})"
+      
+      def add_empty_has_many_subselect(assoc)
+        value = @attrs[assoc.name]
+        not_matching_subselect = @model.construct_finder_sql(
+          :select => "#{@model.table_name}.id", :joins => assoc.name,
+          :group => "#{@model.table_name}.id"
+        )
+        @ar_query.condition_sqls << "id not in (#{not_matching_subselect})"
+      end
+      
+      def add_non_empty_has_many_subselect(assoc)
+        value = @attrs[assoc.name]
+        matching_inner_subselect = @model.construct_finder_sql(
+          :select =>
+            "#{@model.table_name}.id, count(#{assoc.klass.table_name}.id) as count",
+          :joins => assoc.name,
+          :conditions => [
+            "#{assoc.klass.table_name}.id in (?)", value.map(&:id)
+          ],
+          :group => "#{@model.table_name}.id"
+        )
+        matching_subselect =
+          "id in (select matching.id from (#{matching_inner_subselect}) as matching where matching.count = #{value.size})"
+        @ar_query.condition_sqls << matching_subselect
+        not_matching_subselect = @model.construct_finder_sql(
+          :select => "#{@model.table_name}.id", :joins => assoc.name,
+          :conditions => [
+            "#{assoc.klass.table_name}.id not in (?)", value.map(&:id)
+          ],
+          :group => "#{@model.table_name}.id"
+        )
+        @ar_query.condition_sqls << "id not in (#{not_matching_subselect})"
+      end
+      
+      def attach_belongs_to_associations_to_query
+        @model.belongs_to_associations.each do |assoc|
+          if @attrs.keys.include?(assoc.name.to_s)
+            @ar_query.conditions[assoc.primary_key_name] = if @attrs[assoc.name]
+              @attrs[assoc.name].id
+            else
+              @attrs[assoc.name]
+            end
+          end
+        end
+      end
+      
+      def attach_non_associated_attrs_to_query
+        @attrs.each do |k,v|
+          if @model.column_names.include?(k.to_s)
+            @ar_query.conditions[k] = v
+          end
         end
       end
 
       def instance
-        attrs = Sampler.reify_association_hashes @model, @attrs
-        attrs = HashWithIndifferentAccess.new attrs
-        find_query = ARQuery.new
-        attrs.each do |k,v|
-          if @model.column_names.include?(k.to_s)
-            find_query.conditions[k] = v
-          end
-        end
-        @model.belongs_to_associations.each do |assoc|
-          if attrs.keys.include?(assoc.name.to_s)
-            find_query.conditions[assoc.primary_key_name] = if attrs[assoc.name]
-              attrs[assoc.name].id
+        @attrs = Sampler.reify_association_hashes @model, @attrs
+        @attrs = HashWithIndifferentAccess.new @attrs
+        attach_non_associated_attrs_to_query
+        attach_belongs_to_associations_to_query
+        @model.has_many_associations.each do |assoc|
+          if @attrs.keys.include?(assoc.name.to_s)
+            if @attrs[assoc.name].empty?
+              add_empty_has_many_subselect assoc
             else
-              attrs[assoc.name]
+              add_non_empty_has_many_subselect assoc
             end
           end
         end
-        @model.has_many_associations.each do |assoc|
-          if attrs.keys.include?(assoc.name.to_s)
-            add_has_many_subselect attrs[assoc.name], assoc, find_query
-          end
-        end
-        @model.first find_query.to_hash
+        @model.first @ar_query.to_hash
       end
     end
   end
