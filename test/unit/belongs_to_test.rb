ee5c600ab756fcfb0e9c29b9b043c3b8c255c12b
Francis Hwang <francis.hwang@profitably.com>
2011-09-21 20:09:03 -0400

    getting specific about when to create & when to reuse records

--- test/unit/belongs_to_test.rb
+++ test/unit/belongs_to_test.rb
@@ -23,47 +23,82 @@ class BelongsToTest < SampleModelsTestCase
   end
   
   def test_sets_a_custom_value_by_column_name
-    user = User.sample
-    blog_post = BlogPost.sample :user_id => user.id
-    assert_equal user, blog_post.user
+    user = nil
+    assert_difference('User.count') do
+      user = User.sample
+    end
+    assert_difference('BlogPost.count') do
+      assert_no_difference('User.count') do
+        blog_post = BlogPost.sample :user_id => user.id
+        assert_equal user, blog_post.user
+      end
+    end
   end
   
   def test_sets_a_custom_nil_value_by_association_name
-    show = Show.sample :network => nil
-    assert_nil show.network
-    assert_nil show.network_id
+    assert_difference('Show.count') do
+      show = Show.sample :network => nil
+      assert_nil show.network
+      assert_nil show.network_id
+    end
   end  
   
   def test_sets_a_custom_nil_value_by_column_name
-    show = Show.sample :network_id => nil
-    assert_nil show.network
-    assert_nil show.network_id
+    assert_difference('Show.count') do
+      show = Show.sample :network_id => nil
+      assert_nil show.network
+      assert_nil show.network_id
+    end
   end
   
   def test_has_no_problem_with_circular_associations
-    assert User.sample.favorite_blog_post.is_a?(BlogPost)
-    assert BlogPost.sample.user.is_a?(User)
+    assert_difference('User.count') do
+      assert User.sample.favorite_blog_post.is_a?(BlogPost)
+    end
+    assert_difference('BlogPost.count') do
+      assert BlogPost.sample.user.is_a?(User)
+    end
   end
   
   def test_allows_creation_of_a_custom_associated_instance_with_a_hash
-    show = Show.sample(
-      :name => 'The Daily Show', :network => {:name => 'Comedy Central'}
-    )
-    assert_equal "The Daily Show", show.name
-    assert_equal 'Comedy Central', show.network.name
+    assert_difference('Show.count') do
+      assert_difference('Network.count') do
+        show = Show.sample(
+          :name => 'The Daily Show', :network => {:name => 'Comedy Central'}
+        )
+        assert_equal "The Daily Show", show.name
+        assert_equal 'Comedy Central', show.network.name
+      end
+    end
   end
 
   def test_assigns_association_if_a_value_is_passed_in_as_the_first_argument
-    user = User.sample
-    blog_post = BlogPost.sample(user, :title => 'some title')
-    assert_equal user, blog_post.user
-    assert_equal 'some title', blog_post.title
+    user = nil
+    assert_difference('User.count') do
+      user = User.sample
+    end
+    assert_difference('BlogPost.count') do
+      assert_no_difference('User.count') do
+        blog_post = BlogPost.sample(user, :title => 'some title')
+        assert_equal user, blog_post.user
+        assert_equal 'some title', blog_post.title
+      end
+    end
   end
 
   def test_creates_associated_records_with_shortcuts
-    network = Network.sample
-    video = Video.sample(:show => [network, {:name => "Jersey Shore"}])
-    assert_equal network, video.show.network
-    assert_equal "Jersey Shore", video.show.name
+    network = nil
+    assert_difference('Network.count') do
+      network = Network.sample
+    end
+    assert_difference('Video.count') do
+      assert_difference('Show.count') do
+        assert_no_difference('Network.count') do
+          video = Video.sample(:show => [network, {:name => "Jersey Shore"}])
+          assert_equal network, video.show.network
+          assert_equal "Jersey Shore", video.show.name
+        end
+      end
+    end
   end
 end
